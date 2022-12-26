2376501367bb1ca4b5c4d099521f6b71577679f3
Azeez Salu <51140317+azeezsalu@users.noreply.github.com>
2022-12-26 01:01:52 -0500

    Add files via upload

---
+++ docker-projects/rentzone/build_image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run the docker build command
+docker build \
+--build-arg PERSONAL_ACCESS_TOKEN= \
+--build-arg GITHUB_USERNAME= \
+--build-arg REPOSITORY_NAME= \
+--build-arg WEB_FILE_ZIP= \
+--build-arg WEB_FILE_UNZIP= \
+--build-arg DOMAIN_NAME= \
+--build-arg RDS_ENDPOINT= \
+--build-arg RDS_DB_NAME= \
+--build-arg RDS_MASTER_USERNAME= \
+--build-arg RDS_DB_PASSWORD= \
+-t <image-tag> .
