e55d9bd936a15a61d311260982becdeffec2d7dd
Azeez Salu <51140317+azeezsalu@users.noreply.github.com>
2023-12-22 05:00:43 -0500

    Add files via upload

---
+++ docker-projects/rentzone/migrate-data.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+S3_URI=s3://aosnote-rentzone-sql-files/V1__rentzone-db.sql
+RDS_ENDPOINT=dev-rds-db.cu2idoemakwo.us-east-1.rds.amazonaws.com
+RDS_DB_NAME=applicationdb
+RDS_DB_USERNAME=azeezs
+RDS_DB_PASSWORD=azeezs123
+
+# Update all packages
+sudo yum update -y
+
+# Download and extract Flyway
+sudo wget -qO- https://download.red-gate.com/maven/release/com/redgate/flyway/flyway-commandline/10.4.0/flyway-commandline-10.4.0-linux-x64.tar.gz | tar -xvz
+
+# Create a symbolic link to make Flyway accessible globally
+sudo ln -s $(pwd)/flyway-10.4.0/flyway /usr/local/bin
+
+# Create the SQL directory for migrations
+sudo mkdir sql
+
+# Download the migration SQL script from AWS S3
+sudo aws s3 cp "$S3_URI" sql/
+
+# Run Flyway migration
+sudo flyway -url=jdbc:mysql://"$RDS_ENDPOINT":3306/"$RDS_DB_NAME" \
+  -user="$RDS_DB_USERNAME" \
+  -password="$RDS_DB_PASSWORD" \
+  -locations=filesystem:sql \
+  migrate
